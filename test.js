ea99df6e3379c4feccff9445f1213152777af757
Joshua HG <jahg901@gmail.com>
2021-08-03 22:46:13 -0400

    oops did i commit test.js? that file's cringe

--- test.js
+++
@@ -1,6 +0,0 @@
-const dateFormat = new Intl.DateTimeFormat('en-US',
-    { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' });
-
-let d = new Date(2021, 6, 15, 2, 60)
-
-console.log(dateFormat.format(d));
